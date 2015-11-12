44b82b8373314ab6b6f51825a5bb571ac665469c
erickriger <erickriger@gmail.com>
2015-11-12 05:59:36 -0200

    Algoritmo 47

---
+++ algoritmo47/src/main/java/br/ufg/inf/es/construcao/algoritmo47/Algoritmo47.java
@@ -0,0 +1,22 @@
+package br.ufg.inf.es.construcao.algoritmo47;
+
+import br.ufg.inf.es.construcao.algoritmo46.Algoritmo46;
+
+/**
+ * Media do Array.
+ * @author eric
+ */
+public class Algoritmo47 {
+
+    /**
+     * Calcula a media entre os elementos de um array.
+     * @param array Array a ter a media calculada.
+     * @return      A media dos elementos do array.
+     */
+    public static double mediaArray(int[] array) {
+        
+        int soma = Algoritmo46.somaArray(array);
+        
+        return soma / array.length;
+    }
+}
